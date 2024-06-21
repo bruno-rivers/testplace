7989008f7ad1534bf820084bb70df2705b9bc4e1
b-rios <alrios4319@gmail.com>
2024-06-21 17:20:23 -0300

    add model switch

---
+++ src/data/models.js
@@ -0,0 +1,64 @@
+const models = [
+    {
+        model: 'Code Llama Instruct (13B)',
+        modelStringAPI: 'codellama/CodeLlama-13b-Instruct-hf',
+    },
+    {
+        model: 'Code Llama Instruct (34B)',
+        modelStringAPI: 'codellama/CodeLlama-34b-Instruct-hf',
+    },
+    {
+        model: 'Code Llama Instruct (70B)',
+        modelStringAPI: 'codellama/CodeLlama-70b-Instruct-hf',
+    },
+    {
+        model: 'Code Llama Instruct (7B)',
+        modelStringAPI: 'codellama/CodeLlama-7b-Instruct-hf',
+    },
+    {
+        model: 'LLaMA-2 Chat (70B)',
+        modelStringAPI: 'meta-llama/Llama-2-70b-chat-hf',
+    },
+    {
+        model: 'LLaMA-2 Chat (7B)',
+        modelStringAPI: 'meta-llama/Llama-2-7b-chat-hf',
+    },
+    {
+        model: 'LLaMA-2 Chat (13B)',
+        modelStringAPI: 'meta-llama/Llama-2-13b-chat-hf',
+    },
+    {
+        model: 'LLaMA-3 Chat (7B)',
+        modelStringAPI: 'meta-llama/Llama-3-7b-chat-hf',
+    },
+    {
+        model: 'LLaMA-3 Chat (8B)',
+        modelStringAPI: 'meta-llama/Llama-3-8b-chat-hf',
+    },
+    {
+        model: 'LLaMA-3 Chat (70B)',
+        modelStringAPI: 'meta-llama/Llama-3-70b-chat-hf',
+    },
+    {
+        model: 'Mistral (7B) Instruct',
+        modelStringAPI: 'mistralai/Mistral-7B-Instruct-v0.1',
+    },
+    {
+        model: 'Mistral (7B) Instruct v0.2',
+        modelStringAPI: 'mistralai/Mistral-7B-Instruct-v0.2',
+    },
+    {
+        model: 'Mistral (7B) Instruct v0.3',
+        modelStringAPI: 'mistralai/Mistral-7B-Instruct-v0.3',
+    },
+    {
+        model: 'Mixtral-8x7B Instruct (46.7B)',
+        modelStringAPI: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+    },
+    {
+        model: 'Mixtral-8x22B Instruct (141B)',
+        modelStringAPI: 'mistralai/Mixtral-8x22B-Instruct-v0.1',
+    },
+]
+
+export default models;
